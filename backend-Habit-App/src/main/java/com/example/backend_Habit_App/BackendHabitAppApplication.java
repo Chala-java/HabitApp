8155d5c94b54b51db5cc8b1de07f02a661cfc23a
Rawcossio <raulcossio166183@gmail.com>
2025-06-24 16:26:47 -0500

    feat: estructura base del proyecto con carpetas y configuración inicial

---
+++ backend-Habit-App/src/main/java/com/example/backend_Habit_App/BackendHabitAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.backend_Habit_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendHabitAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendHabitAppApplication.class, args);
+	}
+
+}
