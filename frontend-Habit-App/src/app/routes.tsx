d34f7d2dc7308d90320910b6961f461dd76eeb24
Rawcossio <raulcossio166183@gmail.com>
2025-06-25 15:11:57 -0500

    feat(app/routes.tsx): se crea el enrutamiento de las vistas

---
+++ frontend-Habit-App/src/app/routes.tsx
@@ -0,0 +1,8 @@
+import Login from "../ui/components/Login";
+
+export let enrutador = [
+  {
+    path: "/",
+    element:<Login />
+  }
+]
--- frontend-Habit-App/src/main.tsx
+++ frontend-Habit-App/src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter,RouterProvider } from 'react-router-dom'
+import { enrutador } from './app/routes.tsx'
 import './index.css'
-import App from './App.tsx'
+
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider
+      router={createBrowserRouter(enrutador)}/>
   </StrictMode>,
 )
